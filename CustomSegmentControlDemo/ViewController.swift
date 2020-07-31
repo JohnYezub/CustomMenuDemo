1f6cbebba611f34199b7824ad499bf4127627c59
JohnYezub <buze@ya.ru>
2020-07-31 14:18:41 +0800

    Initial Commit

---
+++ CustomSegmentControlDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomSegmentControlDemo
+//
+//  Created by   admin on 31.07.2020.
+//  Copyright © 2020 Evgeny Ezub. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
